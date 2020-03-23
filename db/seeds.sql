3dcc258566575c0ed68e0ae638a85f8f8eaae456
mustafa khairalla <mukhairalla@gmail.com>
2020-03-22 22:46:40 -0600

    upload app files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Regular Burger');
+INSERT INTO burgers (name, devour) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Burin Love', true);
+INSERT INTO burgers (name, devour) VALUES ('Royal Burger', true);
+INSERT INTO burgers (name) VALUES ('Chees Burger');
